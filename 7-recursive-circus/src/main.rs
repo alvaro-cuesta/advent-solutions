baac335eb5500420c98c8709e6a5ba8b799204e4
alvaro-cuesta <alvaro-cuesta@GitHub>
2017-12-23 19:15:44 +0100

    Simplify 7

--- 7-recursive-circus/src/main.rs
+++ 7-recursive-circus/src/main.rs
@@ -2,7 +2,7 @@ extern crate advent;
 #[macro_use] extern crate nom;
 use nom::{ alpha, digit };
 use std::str::FromStr;
-use std::collections::{ HashMap, HashSet };
+use std::collections::HashMap;
 
 #[derive(Clone, PartialEq, Eq, Debug)]
 struct Node<'a> {
@@ -30,47 +30,47 @@ impl<'a> Node<'a> {
     }
 }
 
-named!(parse_name<&[u8], &str>, map_res!(alpha, std::str::from_utf8));
+named!{ parse_name(&[u8]) -> &str,
+    map_res!(alpha, std::str::from_utf8)
+}
 
-named!(parse_node<&[u8], Node>,
-  do_parse!(
-    name: parse_name >>
+named!{ parse_node(&[u8]) -> Node,
+    do_parse!(
+        name: parse_name >>
 
-    weight: map_res!(
-        map_res!(
-            delimited!(tag!(" ("), digit, tag!(")")),
-            std::str::from_utf8
-        ),
-        FromStr::from_str
-    ) >>
+        weight: map_res!(
+            map_res!(
+                delimited!(tag!(" ("), digit, tag!(")")),
+                std::str::from_utf8
+            ),
+            FromStr::from_str
+        ) >>
 
-    children: map!(
-        opt!(do_parse!(
-            tag!(" -> ") >>
-            children: separated_nonempty_list_complete!(tag!(", "), parse_name) >>
-            (children)
-        )),
-        |x| x.unwrap_or_else(|| Vec::new())
-    )>>
+        children: map!(
+            opt!(do_parse!(
+                tag!(" -> ") >>
+                children: separated_nonempty_list_complete!(tag!(", "), parse_name) >>
+                (children)
+            )),
+            |x| x.unwrap_or_else(|| Vec::new())
+        )>>
 
-    tag!("\n") >>
+        tag!("\n") >>
 
-    (Node { name, weight, children })
-  )
-);
+        (Node { name, weight, children })
+    )
+}
 
-named!(parse_nodes<&[u8], Vec<Node>>, many0!(parse_node));
+named!{ parse_nodes(&[u8]) -> Vec<Node>,
+    many0!(parse_node)
+}
 
-fn main() {
-    let input = advent::download_input(2017, 7);
-
-    let nodes = parse_nodes(input.as_bytes())
-        .to_full_result()
-        .expect("Error parsing nodes");
+fn step1<'a, 'b>(nodes: &'a [Node<'b>]) -> &'b str {
+    use std::collections::HashSet;
 
     let mut is_children = HashSet::new();
 
-    for node in &nodes {
+    for node in nodes {
         for &child_name in &node.children {
             is_children.insert(child_name);
         }
@@ -79,23 +79,23 @@ fn main() {
     let mut roots = nodes.iter()
         .filter(|x| !is_children.contains(x.name));
 
-    let step1 = roots.next().expect("No roots found");
+    let root = roots.next().expect("No roots found");
     assert!(roots.next() == None, "Found more than one root");
 
-    println!("Step 1: {}", step1.name);
+    root.name
+}
 
+fn step2(nodes: &[Node]) -> usize {
     let mut by_name = HashMap::new();
 
-    for node in &nodes {
+    for node in nodes {
         by_name.insert(node.name, node);
     }
 
-    let by_name_ref = &by_name;
-
     let imbalance_origins = nodes.iter()
-        .filter(|node| !node.is_balanced(by_name_ref)
+        .filter(|node| !node.is_balanced(&by_name)
             && node.children.iter()
-                .all(|child_name| by_name_ref[child_name].is_balanced(by_name_ref)))
+                .all(|child_name| by_name[child_name].is_balanced(&by_name)))
         .collect::<Vec<_>>();
 
     assert!(imbalance_origins.len() == 1, "Found zero or more than one imbalance origin");
@@ -103,8 +103,8 @@ fn main() {
     let imbalanced_children = imbalance_origins[0]
         .children.iter()
         .map(|child_name| {
-            let node = by_name_ref[child_name];
-            let weight = node.tree_weight(by_name_ref);
+            let node = by_name[child_name];
+            let weight = node.tree_weight(&by_name);
 
             (node, weight)
         })
@@ -123,16 +123,25 @@ fn main() {
             .filter(|&&(_, weight)| weight == max_weight)
             .collect::<Vec<_>>();
 
-        let result = if min_children.len() == 1 {
+        if min_children.len() == 1 {
             min_node.weight + max_weight - min_weight
         } else if max_children.len() == 1 {
             max_node.weight + min_weight - max_weight
         } else {
             panic!("More than one min/max");
-        };
-
-        println!("Step 2: {}", result);
+        }
     } else {
         panic!("Could not find min and max imbalanced children");
     }
+}
+
+fn main() {
+    let input = advent::download_input(2017, 7);
+
+    let nodes = parse_nodes(input.as_bytes())
+        .to_full_result()
+        .expect("Error parsing nodes");
+
+    println!("Step 1: {}", step1(&nodes));
+    println!("Step 2: {}", step2(&nodes));
 }
