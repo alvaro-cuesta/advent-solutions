9d52616af8353d8acfbf046d38c2b19a12f5dd23
alvaro-cuesta <alvaro-cuesta@GitHub>
2017-12-23 18:01:36 +0100

    Simplify day 2

--- 2-corruption-checksum/src/main.rs
+++ 2-corruption-checksum/src/main.rs
@@ -1,5 +1,41 @@
 extern crate advent;
 
+fn step1<'a, I, J>(lines: I) -> u32
+    where I: IntoIterator<Item=J>,
+          J: IntoIterator<Item=&'a u32>,
+{
+    lines.into_iter()
+        .map(|line| match advent::min_and_max(line) {
+            Some((min, Some(max))) => max - min,
+            Some((_, None)) => 0,
+            _ => panic!("Unexpected empty line"),
+        })
+        .sum::<u32>()
+}
+
+fn step2<'a, I, J>(lines: I) -> u32
+    where I: IntoIterator<Item=J>,
+          J: IntoIterator<Item=&'a u32> + Copy,
+{
+    lines.into_iter()
+        .map(|line| {
+            let mut divisible_pairs = line.into_iter()
+                .enumerate()
+                .filter_map(|(i, x)| line.into_iter()
+                    .skip(i + 1)
+                    .find(|&y| (x % y) == 0 || (y % x) == 0)
+                    .map(|y| (x, y))
+                );
+
+            let (x, y) = divisible_pairs.next().expect("No divisible pair found");
+
+            assert!(divisible_pairs.next().is_none(), "More than one divisible pairs");
+
+            if x > y { x / y } else { y / x }
+        })
+        .sum::<u32>()
+}
+
 fn main() {
     let input = advent::download_input(2017, 2);
 
@@ -10,34 +46,6 @@ fn main() {
         )
         .collect::<Vec<_>>();
 
-    let step1 = lines.iter()
-        .map(|line| match advent::min_and_max(line.iter()) {
-            Some((min, Some(max))) => max - min,
-            Some((_, None)) => 0,
-            _ => panic!("Unexpected empty line"),
-        })
-        .sum::<u32>();
-
-    println!("Step 1: {}", step1);
-
-    let step2 = lines.iter()
-        .map(|line| {
-            let mut divisible_pairs = line.iter()
-                .enumerate()
-                .filter_map(|(i, x)| {
-                    line.iter()
-                        .skip(i + 1)
-                        .find(|&y| (x % y) == 0 || (y % x) == 0)
-                        .map(|y| (x, y))
-                });
-
-            let (x, y) = divisible_pairs.next().expect("No divisible pair found");
-
-            assert!(divisible_pairs.next().is_none(), "More than one divisible pairs");
-
-            if x > y { x / y } else { y / x }
-        })
-        .sum::<u32>();
-
-    println!("Step 2: {:?}", step2);
+    println!("Step 1: {}", step1(&lines));
+    println!("Step 2: {}", step2(&lines));
 }
