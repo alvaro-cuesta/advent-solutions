b6475eab91e46a4b6fb030c149c9e1cf42e1b4c0
alvaro-cuesta <alvaro-cuesta@GitHub>
2017-12-24 17:13:02 +0100

    Librarify day 10

--- 10-knot-hash/src/main.rs
+++ 10-knot-hash/src/main.rs
@@ -1,36 +1,6 @@
-extern crate advent;
+#![feature(conservative_impl_trait)]
 
-fn new_nums() -> Vec<u8> {
-    let mut nums = vec![];
-
-    for i in 0..256 {
-        nums.push(i as u8);
-    }
-
-    nums
-}
-
-fn hash(nums: &mut [u8], lengths: &[u8], rounds: usize) {
-    let mut current_pos = 0;
-    let mut skip_size = 0;
-
-    for _ in 0..rounds {
-        for length in lengths {
-            for i in 0..length/2 {
-                let ia = (current_pos + i as usize) % nums.len();
-                let ib = (current_pos + (length - i - 1) as usize) % nums.len();
-
-                nums.swap(ia, ib);
-            }
-
-            current_pos += (*length as usize) + skip_size;
-            current_pos %= nums.len();
-
-            skip_size += 1;
-            skip_size %= nums.len();
-        }
-    }
-}
+extern crate knot_hash;
 
 fn step1(input: &str) -> u16 {
     let lengths = input
@@ -38,24 +8,14 @@ fn step1(input: &str) -> u16 {
         .map(|l| l.parse::<u8>().expect("Unexpected non-u8 length"))
         .collect::<Vec<_>>();
 
-    let mut nums = new_nums();
-    hash(&mut nums, &lengths, 1);
+    let mut hash = knot_hash::hash_lengths(&lengths, 1);
 
-    (nums[0] as u16) * (nums[1] as u16)
+    (hash[0] as u16) * (hash[1] as u16)
 }
 
 fn step2(input: &str) -> String {
-    let mut lengths = input.as_bytes().to_vec();
-    lengths.extend([17, 31, 73, 47, 23].iter());
-
-    let mut nums = new_nums();
-    hash(&mut nums, &lengths, 64);
-
-    nums.chunks(16)
-        .map(|chunk| {
-            let x = chunk.iter().fold(0, |state, x| state ^ x);
-            format!("{:02x}", x)
-        })
+    knot_hash::hash_str(input).iter()
+        .map(|x| format!("{:02x}", x))
         .collect::<String>()
 }
 
